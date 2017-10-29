43cfc7fde311e37823b43025054e6689959ed951
Cole Stasney <mkcoarng@gmail.com>
2017-10-29 10:35:24 -0600

    4

--- frontend/html/main.js
+++ frontend/html/main.js
@@ -2,73 +2,47 @@ document.getElementById("checkKill").addEventListener('click', checkKill)
 document.getElementById("startWlan").addEventListener('click', startWlan)
 document.getElementById("airodWlan1").addEventListener('click', airodWlan1)
 
-
-
 function checkKill() {
     fetch('/checkkill/kill', {
         method: 'get'
     })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(function (err) {
-        console.log(err);
-    });
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(function (err) {
+            console.log(err);
+        });
 }
-
-// function checkKill() {
-//     var url = "/checkkill/kill";
-//     return new Promise(function (resolve, reject) {
-//         let xmlhttp = new XMLHttpRequest();
-//         xmlhttp.onreadystatechange = function () {
-//             if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-//                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-//                     myObj = this.responseText;
-//                     console.log(this.response);
-//                 }
-//                 else if (xmlhttp.status == 400) {
-//                     reject('Status code: ' + this.status + "  ERROR");
-//                 }
-
-//                 else {
-//                     reject('Status code: ' + this.status);
-//                 }
-//             }
-//         };
-//         xmlhttp.open("GET", url, true);
-//         xmlhttp.send();
-//     });
-// }
 
 function checkInterface() {
     fetch('/checkinterface/check', {
         method: 'get'
-    }) .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(function (err) {
-        console.log(err);
-    });
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 function startWlan() {
     fetch('/startwlan/start', {
         method: 'get'
-    }) .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(function (err) {
-        console.log(err);
-    });
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 function airodWlan1() {
     fetch('/airodwlan1/airodumpStart', {
         method: 'get'
-    }) .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(function (err) {
-        console.log(err);
-    });
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
-function showError(error) {
-    console.log(error + this.status);
-}
--- routes/airodwlan1.js
+++ routes/airodwlan1.js
@@ -1,12 +1,40 @@
 const express = require('express');
-const router = express.Router();
 const execa = require('execa');
+const fs = require('fs');
+const CsvReadableStream = require('csv-reader');
+const getStream = require('get-stream');
+const router = express.Router();
 
 router.get('/airodumpStart', function (req, res, next) {
-    execa.shell(`airodump-ng wlan0mon`, {maxBuffer: 1500 * 1500}).then(result => {
-        console.log(result.stdout);
-        res.json(result.stdout);
+    const options = {
+        timeout: 6000,
+        maxBuffer: 90000000,
+        killSignal: 'SIGKILL'
+    }
+
+    return new Promise(function (resolve, reject) {
+        execa.shell(`airodump-ng -w /home/pi/Desktop/scanData/ wlan1mon`, options)
+            .then(result => {
+                console.log(result.stdout);
+                resolve(res.json(result.stdout));
+            })
+            .catch(error => {
+                console.log(error);
+                reject(error);
+            });
     })
+        .then(value => {
+            var inputStream = fs.createReadStream('/home/pi/Desktop/scanData/-01.csv', 'utf8');
+
+            inputStream
+                .pipe(CsvReader({ parseNumbers: true, parseBooleans: true, trim: true }))
+                .on('data', function (row) {
+                    console.log('A row arrived: ', row);
+                })
+                .on('end', function (data) {
+                    console.log('No more rows!');
+                });
+        })
         .catch(error => {
             console.log(error);
         });
